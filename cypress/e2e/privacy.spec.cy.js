ed90b03c28cce78156f1a4ac61897959c4ef5907
Guilherme de Almeida Pastl <guilherme.pastl@seventh.com.br>
2023-03-22 00:59:15 -0300

    Adição de teste de API 2

--- cypress/e2e/privacy.spec.cy.js
+++ cypress/e2e/privacy.spec.cy.js
@@ -1,4 +1,6 @@
-it.only('Testa a página da política de privacidade de forma independente', () => {
-    cy.visit('./src/privacy.html')
-    cy.contains('Talking About Testing').should('be.visible')
+Cypress._.times(5, () => {
+    it.only('Testa a página da política de privacidade de forma independente', () => {
+        cy.visit('./src/privacy.html')
+        cy.contains('Talking About Testing').should('be.visible')
+    })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,8 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('fillMandatoryFieldsAndSumit', () => {
+    cy.get('#firstName').type('Guilherme')
+    cy.get('#lastName').type('Pastl')
+    cy.get('#email').type('gui@exemplo.com')
+    cy.get('#open-text-area').type('texto exemplo abcd!@123')
+    cy.get('.button[type="submit"]').click()
+
+})
